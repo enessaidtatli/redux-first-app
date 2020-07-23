7b642fd1554ff2d891a42baee14432af2288d1bf
enessaidtatli <enes.tatli@cfn.com.tr>
2020-07-23 16:25:47 +0300

    added redux properties

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+import counterReducer from "./counterReducer";
+
+const reducers = combineReducers({
+  counterReducer,
+});
+
+// const reducers = combineReducers({
+//   counterReducer: counterReducer
+// });
+
+export default reducers;
